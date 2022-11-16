1b65be881e0aeaf8982e7fe41b0bdf4dc1d61b06
Zhanna Shchanova <50460686+zvshchanova@users.noreply.github.com>
2022-11-16 19:15:02 +0200

    change app.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default function App() {
   const data = useFetch(url, lat, long);
   return (
     <div className="App">
-      <h1>Hello! </h1>
+      <h1>Hello ! </h1>
       {typeof data !== "undefined" ? <>{data}</> : <></>}
     </div>
   );
